0b8e8f62a61e5a72af33d721aae942ed058d0e4c
Kamile Pimenta <kamile.pimenta@outlook.com>
2021-09-23 01:47:50 -0300

    Including assests and data

---
+++ src/data/characters.js
@@ -0,0 +1,36 @@
+const characters = {
+    data: [
+        {
+            name: 'Batman',
+            history: '<p>A identidade secreta de Batman é Bruce Wayne, um bilionário americano, playboy, magnata de negócios, filantropo e dono da corporação Wayne Enterprises.</p><p>Depois de testemunhar o assassinato dos seus pais enquanto criança, Wayne jurou vingança contra os criminosos. Bruce treina então física e intelectualmente, e cria uma persona inspirada no morcego para combater o crime.</p>',
+            color: 'yellow',
+        },
+        {
+            name: 'Flash',
+            history: '<p>Em uma noite chuvosa, o policial forense Barry Allen está em seu laboratório lendo as histórias do Flash Jay Garrick, se perguntando como seria ter o poder da velocidade. Enquanto arruma a prateleira com produtos químicos de seu laboratório, o acelarador de partículas explode e ele é atingido por um raio que também faz com que os produtos químicos caiam em cima dele.</p><p>Apesar de ter sido atingido por um raio e ter o corpo banhado por produtor químicos, Barry sai completamente ileso de qualquer ferimento.</p>',
+            color: 'orange',
+        },
+        {
+            name: 'Wonder Woman',
+            history: '<p>Diana nasceu e cresceu na ilha escondida de Themyscira, lar da raça Amazona de mulheres guerreiras criadas pelos deuses do Monte Olimpo para proteger a humanidade contra a corrupção de Ares, o Deus da Guerra. No passado distante, Ares matou todos os olímpicos, mas seu pai mortalmente ferido, Zeus, o derrotou.</p><p>Quando jovem, Diana resgata o capitão Steve Trevor, um piloto das Forças Expedicionárias Americanas, depois que seu avião cai fora da costa de Themyscira.</p>',
+            color: 'yellow',
+        },
+        {
+            name: 'Superman',
+            history: '<p>Aos 18 anos, logo após a morte de Jonathan devido a um ataque cardíaco, Clark ouve um "chamado" psíquico e descobre um cristal brilhante nos restos de sua espaçonave. Ele o obriga a viajar para o Ártico, onde o cristal constrói a Fortaleza da Solidão. No interior, uma visão holográfica de Jor-El aparece e explica as origens de Clark, educando-o sobre seus poderes e responsabilidades.</p><p>Depois de 12 anos de treinamento, com seus poderes totalmente desenvolvidos, ele deixa a Fortaleza vestindo um terno azul e vermelho com o brasão da família Casa de El em seu peito</p>',
+            color: 'blue',
+        },
+        {
+            name: 'Cyborg',
+            history: '<p>Victor Stone é um ex-atleta universitário e estrela do futebol. Após um acidente horrível, seu pai Silas Stone, conseguiu salvá-lo através do uso de uma Caixa Materna que criou cibernética nele. Victor então ficou conhecido como o super-herói Ciborgue.</p><p>Reduzido a nada mais do que um torso, cabeça e metade de seus braços, Victor foi mantido vivo por seu pai, que tentou todo tipo de procedimento médico para ajudar a curá-lo.</p>',
+            color: 'red',
+        },
+        {
+            name: 'Aquaman',
+            history: '<p>Arthur Curry é um híbrido meio-humano/meio-atlante, que está destinado à ser o rei da nação submarina de Atlântida, com a capacidade de manipular as marés do oceano, se comunicar com outras vidas aquáticas e nadar em velocidades supersônicas.</p><p>Arthur vence a guerra contra Orm, assumindo o trono de Atlantis que é seu por direito e assim governa o seu povo, pacífico e justo.</p>',
+            color: 'marine',
+        },
+    ],
+};
+
+export default characters;
